23033ba7e93f9ef603ea52eeab4bb86c8a538410
Shirakawa <673254896@qq.com>
2018-04-14 15:40:49 +0800

    Create Registry2.py

---
+++ CasterServiceRegistry/Registry2.py
@@ -0,0 +1,42 @@
+import socket
+
+def printRedChr(s):
+    print "\033[1;31;40m"+s+"\033[0m"
+
+def conn_redis(redis_host = 'localhost',redis_port = '6379',redis_db = '15'):
+    return redis.StrictRedis(host=redis_host,port=redis_port,db=redis_db) 
+    
+def get_ip():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print hostname , ip
+    return ip
+
+if __name__ == "__main__":
+    try :
+        print 'Redis'
+        rs = conn_redis(redis_host = 'redis',redis_db = '3')
+        print rs.keys('*')
+        print 'Done'
+    except Exception, e:
+        printRedChr('redis ERR!')
+        print e
+        exit(1)
+    try :
+        ip = get_ip()
+    except Exception, e:
+        printRedChr('getIp ERR!')
+        print e
+        exit(1)
+    try :
+        port = 1005
+        upstream = 'http://'+ip+':'+str(port)
+        rs.sadd('upstream',ip)
+        print str(rs.smembers('upstream'))
+        print 'Registry success!'
+    except Exception, e:
+        printRedChr('Registry ERR!')
+        exit(1)
+
+    printRedChr('Registry Done.')
+    exit(0)
